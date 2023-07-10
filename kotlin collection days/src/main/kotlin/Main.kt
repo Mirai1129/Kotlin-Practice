1819dfbe4856a50e2e73a39d674331929c816c18
Mirai1129 <minecraftmiku831@gmail.com>
2023-07-10 15:05:46 +0800

    how to use function

--- kotlin collection days/src/main/kotlin/Main.kt
+++ kotlin collection days/src/main/kotlin/Main.kt
@@ -1,5 +1,15 @@
 fun main() {
-    print("Your name:")
-    val name = readln()
-    println("Hello $name")
+    val myName: String = "Mirai"
+    val myId: Int = 1129
+    val targetArgument = callName(myName)
+
+    printOnTerminal(targetArgument)
+}
+
+fun callName(name: String = "everyone"): String {
+    return "Hello $name"
+}
+
+fun printOnTerminal(arg: String) {
+    println(arg)
 }
