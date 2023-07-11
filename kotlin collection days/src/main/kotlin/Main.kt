f4da700d6d111a0efbbdb65a169e4e07674883ed
Mirai1129 <minecraftmiku831@gmail.com>
2023-07-11 16:10:04 +0800

    day 7 array practice

--- kotlin collection days/src/main/kotlin/Main.kt
+++ kotlin collection days/src/main/kotlin/Main.kt
@@ -1,29 +1,16 @@
 fun main() {
-    whenCondition()
-    forScope()
-}
+    val arrayOfNumbers = arrayOf(2, 3, 5, 6, 10)
+    val intArray = intArrayOf(2, 3, 5, 10)
+    val size = arrayOfNumbers.size // 以屬性取得 Array 的大小
+    val sum = arrayOfNumbers.sum() // 計算出 Array 裡所有數字的總合
+    val average = arrayOfNumbers.average() // 計算出 Array 裡所有數字的平均值
+    val first = arrayOfNumbers.first() // 取出第一格資料
+    val last = arrayOfNumbers.last() // 取出最後一格資料
+    val min = arrayOfNumbers.minOrNull() // 取出 Array 裡的最小值或 Null
+    val max = arrayOfNumbers.maxOrNull() // 取出 Array 裡的最大值或 Null
+    val firstValue = arrayOfNumbers[0] // 取得 index 是 0 的值
 
-fun whenCondition() {
-    println("x = ???")
-    // when 表達式
-    when (readln().toInt()) {
-        1 -> {
-            println("x == 1")
-        }
-        2 -> {
-            println("x == 2")
-        }
-        else -> {
-            println("x is neither 1 nor 2")
-        }
-    }
-}
-
-fun forScope() {
-    // for 迴圈
-    val items = intArrayOf(1, 2, 3, 4, 5)
-
-    for (item in items) {
-        println(item)
+    for (item in arrayOfNumbers) {
+        println(item) // 會一行一行印出 2 3 5 6 10
     }
 }
