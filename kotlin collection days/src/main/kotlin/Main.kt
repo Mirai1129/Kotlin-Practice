b1a0230ad2bb1e68363a041651d11d659df1231a
Mirai1129 <minecraftmiku831@gmail.com>
2023-07-14 11:00:12 +0800

    day 8：Kotlin Collection of List

--- kotlin collection days/src/main/kotlin/Main.kt
+++ kotlin collection days/src/main/kotlin/Main.kt
@@ -1,16 +1,20 @@
 fun main() {
-    val arrayOfNumbers = arrayOf(2, 3, 5, 6, 10)
-    val intArray = intArrayOf(2, 3, 5, 10)
-    val size = arrayOfNumbers.size // 以屬性取得 Array 的大小
-    val sum = arrayOfNumbers.sum() // 計算出 Array 裡所有數字的總合
-    val average = arrayOfNumbers.average() // 計算出 Array 裡所有數字的平均值
-    val first = arrayOfNumbers.first() // 取出第一格資料
-    val last = arrayOfNumbers.last() // 取出最後一格資料
-    val min = arrayOfNumbers.minOrNull() // 取出 Array 裡的最小值或 Null
-    val max = arrayOfNumbers.maxOrNull() // 取出 Array 裡的最大值或 Null
-    val firstValue = arrayOfNumbers[0] // 取得 index 是 0 的值
+    val listOfNumbers = listOf(2, 3, 5, 6, 7) // 容量與內容都不可變的 List
+    val mutableListOfNumbers = mutableListOf(2, 3, 5, 6, 7) // 可以動態改變容量及內容的 MutableList
+    val emptyList = listOf<Int>()
+    val emptyMutableList = mutableListOf<Int>()
+    val emptyListByEmptyList = emptyList<Int>()
 
-    for (item in arrayOfNumbers) {
-        println(item) // 會一行一行印出 2 3 5 6 10
+    val firstValue = listOfNumbers[0] // 通常用這個方式, [] 是 operator, override get 方法
+    val secondValue = listOfNumbers.get(2)
+
+    val size = listOfNumbers.size // 以屬性取得 List 的大小
+    val sum = listOfNumbers.sum() // 計算出 List 裡所有數字的總合
+    val average = listOfNumbers.average() // 計算出 List 裡所有數字的平均值
+    val min = listOfNumbers.minOrNull() // 取出 List 裡的最小值或 Null
+    val max = listOfNumbers.maxOrNull() // 取出 List 裡的最大值或 Null
+
+    for (item in listOfNumbers) {
+        println(item) // 會一行一行印出 2 3 5 6 7
     }
 }
