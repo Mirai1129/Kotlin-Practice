c446b0890ad9ba7923d38097f72e46396a6add1e
Mirai1129 <minecraftmiku831@gmail.com>
2023-07-14 13:23:17 +0800

    day 9：Kotlin Collection of Set

--- kotlin collection days/src/main/kotlin/Main.kt
+++ kotlin collection days/src/main/kotlin/Main.kt
@@ -1,20 +1,21 @@
 fun main() {
-    val listOfNumbers = listOf(2, 3, 5, 6, 7) // 容量與內容都不可變的 List
-    val mutableListOfNumbers = mutableListOf(2, 3, 5, 6, 7) // 可以動態改變容量及內容的 MutableList
-    val emptyList = listOf<Int>()
-    val emptyMutableList = mutableListOf<Int>()
-    val emptyListByEmptyList = emptyList<Int>()
+    val setOfNames = setOf("Jim", "Sue", "Sue", "Nick", "Nick") // 只存放不重複的 Jim, Sue, Nick
+    val mutableSetOfNames = mutableSetOf("Jim", "Sue", "Sue", "Nick", "Nick") // 只存放不重複的 Jim, Sue, Nick
 
-    val firstValue = listOfNumbers[0] // 通常用這個方式, [] 是 operator, override get 方法
-    val secondValue = listOfNumbers.get(2)
+    val emptySet = setOf<String>()
+    val emptyMutableSet = mutableSetOf<String>()
+    val emptySetByEmptySet = emptySet<String>()
 
-    val size = listOfNumbers.size // 以屬性取得 List 的大小
-    val sum = listOfNumbers.sum() // 計算出 List 裡所有數字的總合
-    val average = listOfNumbers.average() // 計算出 List 裡所有數字的平均值
-    val min = listOfNumbers.minOrNull() // 取出 List 裡的最小值或 Null
-    val max = listOfNumbers.maxOrNull() // 取出 List 裡的最大值或 Null
+    val secondName = setOfNames.elementAt(1) // 取得放在第 2 格的元素
 
-    for (item in listOfNumbers) {
-        println(item) // 會一行一行印出 2 3 5 6 7
+    for (name in setOfNames) {
+        println(name) // 會一行一行印出 Jim Sue Nick
     }
+
+    val setOfNumbers = setOf(2, 3, 3, 6, 7, 10)
+    val size = setOfNumbers.size // 以屬性取得 Set 的大小
+    val sum = setOfNumbers.sum() // 計算出 Set 裡所有數字的總合
+    val average = setOfNumbers.average() // 計算出 Set 裡所有數字的平均值
+    val min = setOfNumbers.minOrNull() // 取出 Set 裡的最小值或 Null
+    val max = setOfNumbers.maxOrNull() // 取出 Set 裡的最大值或 Null
 }
