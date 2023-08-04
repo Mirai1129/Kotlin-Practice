9a9f682e2343765f2c623b168fdeddf082d1878f
Mirai1129 <minecraftmiku831@gmail.com>
2023-08-04 11:00:34 +0800

    day 10：Kotlin Collection 之 Map

--- kotlin collection days/src/main/kotlin/Main.kt
+++ kotlin collection days/src/main/kotlin/Main.kt
@@ -1,21 +1,34 @@
 fun main() {
-    val setOfNames = setOf("Jim", "Sue", "Sue", "Nick", "Nick") // 只存放不重複的 Jim, Sue, Nick
-    val mutableSetOfNames = mutableSetOf("Jim", "Sue", "Sue", "Nick", "Nick") // 只存放不重複的 Jim, Sue, Nick
+    val mapOfFruit = mapOf("Apple" to 3, "Banana" to 5, "Orange" to 7)
+    val mutableMapOfFruit = mutableMapOf("Apple" to 7, "Banana" to 5, "Orange" to 7)
+//    上面可以寫成下面
+//    val setOfFruit = mapOf(
+//        Pair("Apple", 7),
+//        Pair("Banana", 5),
+//        Pair("Orange", 7)
+//    )
+//    val mutableMapOfFruit = mutableMapOf(
+//        Pair("Apple", 7),
+//        Pair("Banana", 5),
+//        Pair("Orange", 7)
+//    )
 
-    val emptySet = setOf<String>()
-    val emptyMutableSet = mutableSetOf<String>()
-    val emptySetByEmptySet = emptySet<String>()
+    val emptyMap = mapOf<String, Int>() // 型別是 Map<String, Int>
+    val mutableEmptyMap = mutableMapOf<String, Int>() // 型別是 MutableMap<String, Int>
+    val emptyMapByEmptyMap = emptyMap<String, Int>() // 型別是 Map<String, Int>
 
-    val secondName = setOfNames.elementAt(1) // 取得放在第 2 格的元素
+    val price = mapOfFruit["Apple"] // 取得 Key 是 Apple 的值
 
-    for (name in setOfNames) {
-        println(name) // 會一行一行印出 Jim Sue Nick
+    for (fruit in mapOfFruit) {
+        println("${fruit.key} (${fruit.value})")
+        // 會依序印出
+        // Apple (3)
+        // Banana (5)
+        // Orange (7)
     }
 
-    val setOfNumbers = setOf(2, 3, 3, 6, 7, 10)
-    val size = setOfNumbers.size // 以屬性取得 Set 的大小
-    val sum = setOfNumbers.sum() // 計算出 Set 裡所有數字的總合
-    val average = setOfNumbers.average() // 計算出 Set 裡所有數字的平均值
-    val min = setOfNumbers.minOrNull() // 取出 Set 裡的最小值或 Null
-    val max = setOfNumbers.maxOrNull() // 取出 Set 裡的最大值或 Null
+    val size = mapOfFruit.size // 以屬性取得 Map 的大小
+    val count = mapOfFruit.count() // 以方法取得 Map 的大小
+    val keys = mapOfFruit.keys // 以屬性取得 Map 的所有 Key
+    val value = mapOfFruit.values // 以屬性取得 Map 的所有 Value
 }
